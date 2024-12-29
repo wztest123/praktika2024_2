9f5eb75cecf21b20b2c387e0553ce7543fe8fb7e
wztest123 <114847338+wztest123@users.noreply.github.com>
2024-12-29 22:12:06 +0300

    Delete keis_4 directory

--- keis_4/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
